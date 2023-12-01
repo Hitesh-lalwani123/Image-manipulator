3d3203b5952df431a1ae0447ae49e410b5442acc
Hitesh <hlalwani28022001@gmail.com>
2023-12-01 23:58:29 +0530

    added app page and various routes for resize and compress:

---
+++ frontend/src/components/Resizer.js
@@ -0,0 +1,102 @@
+
+import { useState } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+const Resizer = () => {
+  const [file, setFile] = useState(null);
+  const [height, setHeight] = useState(256);
+  const [width, setWidth] = useState(256);
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  const [message, setMessage] = useState();
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/download", {
+        responseType: "blob", // Important: Set the responseType to 'blob'
+      });
+
+      // Create a blob URL and initiate a download
+      const blob = new Blob([response.data], {
+        type: response.headers["content-type"],
+      });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "image.jpg";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Download error:", error);
+    }
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("image", file);
+    axios
+      .post("http://localhost:5000/uploads", formData, config)
+      .then((res) => {
+        console.log(res);
+        setMessage(res.data);
+        axios.post("http://localhost:5000/resize", { height, width });
+      })
+      .then((response) => {
+        setLoading(false);
+      });
+  };
+  const [loading, setLoading] = useState(false);
+
+  const [imagePreview, setImagePreview] = useState(null);
+  return (
+    <div>
+        <Navbar/>
+      <h1> Upload your images here</h1>
+      {/* <form action="http://localhost:5000/uploads" method="POST" enctype="multipart/form-data"> */}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+          // const fileReader = new FileReader();
+          // let image = URL.createObjectURL(e.target.files[0]);
+
+          // fileReader.readAsDataURL(image);
+          let file = e.target.files[0];
+          if (file) {
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+              setImagePreview(reader.result);
+            };
+
+            reader.readAsDataURL(file);
+          }
+        }}
+      />
+      Height:{" "}
+      <input type="number" onChange={(e) => setHeight(e.target.value)} />
+      Widht: <input type="number" onChange={(e) => setWidth(e.target.value)} />
+      <button type="submit" onClick={handleSubmit}>
+        Generate
+      </button>
+      {imagePreview && (
+        <img
+          src={imagePreview}
+          alt="Preview"
+          style={{ maxWidth: "100%", maxHeight: "200px" }}
+        />
+      )}
+      <button onClick={handleDownload}>Download</button>
+      <div>{message}</div>
+    </div>
+  );
+};
+
+export default Resizer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Compressor from "./components/Compressor";
+import Resizer from "./components/Resizer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/resize" element={<Resizer />} />
+        <Route path="/compress" element={<Compressor />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
